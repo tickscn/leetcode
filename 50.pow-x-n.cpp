28cab982861ab28724face5efb1e686b64197d2d
ticks <xiehuiwu1996@gmail.com>
2019-06-30 17:28:28 +0800

    50.pow-x-n

---
+++ 50.pow-x-n.cpp
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode id=50 lang=cpp
+ *
+ * [50] Pow(x, n)
+ */
+class Solution
+{
+public:
+	double myPow(double x, int n)
+	{
+		if (n == 0) return 1;
+		double half = myPow(x, n / 2);
+		if (n % 2 == 0) return half * half;
+		if (n > 0) return half * half * x;
+		return half * half / x;
+	}
+};
