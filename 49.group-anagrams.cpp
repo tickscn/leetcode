661c24e0f866f10de8773646b2567b07f5f097d2
ticks <xiehuiwu1996@gmail.com>
2019-06-30 17:28:08 +0800

    49.group-anagrams

---
+++ 49.group-anagrams.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=49 lang=cpp
+ *
+ * [49] Group Anagrams
+ */
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+class Solution
+{
+public:
+	vector<vector<string>> groupAnagrams(vector<string>& strs)
+	{
+		unordered_map<string, vector<string>> map;
+		vector<vector<string>> res;
+		string charMap;
+		for (string str : strs)
+		{
+			charMap = "";
+			vector<int> cnt(26, 0);
+			for (char ch : str)
+			{
+				++cnt[ch - 'a'];
+			}
+			for (int i : cnt)
+			{
+				charMap += to_string(i);
+			}
+			map[charMap].push_back(str);
+		}
+		for (auto out : map)
+		{
+			res.push_back(out.second);
+		}
+		return res;
+	}
+};
